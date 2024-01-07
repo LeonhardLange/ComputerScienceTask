eaab64f967bba9cd0f2e72fa18526835de899283
LeonhardLange <147375006+LeonhardLange@users.noreply.github.com>
2024-01-07 21:05:10 +0100

    A lot of changes

--- src/MrThompson/Analytics.java
+++ src/MrThompson/Analytics.java
@@ -1,9 +1,28 @@
 package MrThompson;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Analytics {
+public class Analytics extends JFrame {
     private JSpinner spinner1;
     private JSpinner spinner2;
     private JButton analyseButton;
+    private JPanel analy;
+
+    public Analytics() {
+        setContentPane(analy);
+        setTitle("Menu");
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        analyseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int minspin = (int) spinner1.getValue();
+                int maxspin = (int) spinner2.getValue();
+                
+            }
+        });
+    }
 }
--- src/MrThompson/MainGUI.java
+++ src/MrThompson/MainGUI.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MainGUI extends JFrame {
+public class MainGUI extends PAndE {
     private JButton gradesButton;
     private JButton participationAndEngagementButton;
     private JButton attendanceButton;
